0f7557130c2158efa26fdce6c1eacb86f5fd1570
vijaya-a-code <thalarivijayalakshmi28@gmail.com>
2025-06-17 10:50:31 +0530

    Add files via upload

---
+++ two.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class two {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int num=s.nextInt();
+        
+    }
+}
