32134e86d169195480e905608f846b04dabface4
vijaya-a-code <thalarivijayalakshmi28@gmail.com>
2025-06-18 21:19:19 +0530

    Create packagedemo.java

---
+++ packagedemo.java
@@ -0,0 +1,13 @@
+import arithmatic.Arithmaticoperations;
+class packagedemo
+{
+public static void main(String args[])
+{
+Arithmaticoperations obj=new Arithmaticoperations();
+System.out.println("sum="+obj.add(2,5));
+System.out.println("sub="+obj.sub(3,5));
+System.out.println("mul="+obj.mul(2,3));
+System.out.println("div="+obj.div(1,2));
+System.out.println("rem="+obj.rem(3,2));
+}
+}
