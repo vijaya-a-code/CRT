46e1e21499d90c1d11128bf4e7804871cdaa4568
vijaya-a-code <thalarivijayalakshmi28@gmail.com>
2025-06-18 21:11:12 +0530

    Create Rain.java

---
+++ Rain.java
@@ -0,0 +1,12 @@
+public class Rain {
+
+	public static void main(String[] args) {
+		boolean isRaining=true;
+		if(isRaining) {
+			System.out.println("take an umbrella");
+		} else {
+			System.out.println(" no need of an a umbrella");
+		}
+	}
+
+}
