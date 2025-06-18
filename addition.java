91b4e28b01ac19e2ae5748f463af28b750ddb186
vijaya-a-code <thalarivijayalakshmi28@gmail.com>
2025-06-18 21:16:24 +0530

    addition.java

---
+++ addition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Addition{
+public static void main(String[]args){
+Scanner sin=new Scanner(System.in);
+int a,b,c;
+System.out.println("enter a:");
+a=sin.nextInt();
+System.out.println("enter b:");
+b=sin.nextInt();
+c=a+b;
+System.out.println("addition of "+a+"and"+b+"is"+c);
+}
+}
