7e625b12cefd8682e341f6dc5eb3d2437d0c067d
vijaya-a-code <thalarivijayalakshmi28@gmail.com>
2025-06-18 21:08:32 +0530

    PalindromeChecker.java

---
+++ PalindromeChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word to check if it's a palindrome: ");
+        String input = sc.nextLine();
+
+        StringBuilder reversed = new StringBuilder(input).reverse();
+
+        if (input.equalsIgnoreCase(reversed.toString())) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+}
