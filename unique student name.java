ee9f7cb0e9b7a4ea32edd01794e18b6cc4ab2b66
vijaya-a-code <thalarivijayalakshmi28@gmail.com>
2025-06-18 21:19:57 +0530

    unique student name.java

---
+++ unique student name.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.scanner
+public class StudentSet {
+public static void main(String[] args){
+HashSet<String>Students=new HashSet<>();
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter 5 Student names:");
+for(int i=0;i<5;i++){
+String name=sc.nextLine();
+Students.add(name);
+System.out.println("Unique Student Names:"+Student);
+}
+}
+}
