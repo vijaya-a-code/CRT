c3cb0783129c3e96c2dc2ae84bf12d6e73bce23d
vijaya-a-code <thalarivijayalakshmi28@gmail.com>
2025-06-19 18:03:35 +0530

    BinaryTree4.java

---
+++ BinaryTree4.java
@@ -0,0 +1,23 @@
+class Node {
+}
+
+public class BinaryTree {
+    Node root;
+    int sumOfNodes(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.data + sumOfNodes(node.left) + sumOfNodes(node.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        System.out.println("Sum of all nodes: " + tree.sumOfNodes(tree.root));  // Output: 15
+    }
+}
